6c095b4e90e23a231c713ec898cfc3cf7dc3b359
AlwinSunil <alv12alwin@gmail.com>
2021-03-27 12:46:49 +0530

    v1

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,22 @@
+const screenWidth = screen.width;
+console.log(screenWidth);
+
 window.onload = function () {
-    warningBanner();
-    window.scrollTo(0, 0);
-  };
+  warningBanner();
+  window.scrollTo(0, 0);
+};
 
 function warningBanner() {
   document.getElementById("warning-banner").style.display = "block";
-  setTimeout(() => {
-    document.getElementById("warning-banner").style.height = "8vh";
-  }, 200);
+  if (screenWidth < 750) {
+    setTimeout(() => {
+      document.getElementById("warning-banner").style.height = "7.5vh";
+    }, 200);
+  } else {
+    setTimeout(() => {
+      document.getElementById("warning-banner").style.height = "4vh";
+    }, 200);
+  }
   setTimeout(() => {
     document.getElementById("warning-banner-p").style.display = "block";
   }, 300);
@@ -18,11 +27,21 @@ function pageReload() {
   setTimeout(() => {
     document.getElementById("container").style.opacity = "60%";
   }, 500);
+  if (screenWidth < 750) {
+    setTimeout(() => {
+      document.getElementById("warning-banner").style.height = "10vh";
+    }, 200);
+  } else {
+    setTimeout(() => {
+      document.getElementById("warning-banner").style.height = "7vh";
+    }, 200);
+  }
+  // setTimeout(() => {
+  //   document.getElementById("warning-banner").style.height = "10vh";
+  // }, 200);
   setTimeout(() => {
-    document.getElementById("warning-banner").style.height = "12vh";
-  }, 200);
-  setTimeout(() => {
-    document.getElementById("warning-banner-dismiss-btn").style.display = "block";
+    document.getElementById("warning-banner-dismiss-btn").style.display =
+      "block";
   }, 100);
 }
 
@@ -31,9 +50,19 @@ function fadeIn() {
     document.getElementById("container").style.opacity = "100%";
   }, 300);
   setTimeout(() => {
-    document.getElementById("warning-banner-dismiss-btn").style.display = "none";
+    document.getElementById("warning-banner-dismiss-btn").style.display =
+      "none";
   }, 100);
-  setTimeout(() => {
-    document.getElementById("warning-banner").style.height = "8vh";
-  }, 100);
+  if (screenWidth < 750) {
+    setTimeout(() => {
+      document.getElementById("warning-banner").style.height = "7.5vh";
+    }, 200);
+  } else {
+    setTimeout(() => {
+      document.getElementById("warning-banner").style.height = "4vh";
+    }, 200);
+  }
+  // setTimeout(() => {
+  //   document.getElementById("warning-banner").style.height = "7vh";
+  // }, 100);
 }
